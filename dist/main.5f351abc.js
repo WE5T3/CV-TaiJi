97e43e134ba62a1b0119c48f51fde29ae2ea3a83
WE5T3 <1024427426@qq.com>
2021-05-05 16:14:52 +0800

    VC-太极 v1.0

---
+++ dist/main.5f351abc.js
@@ -0,0 +1,307 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+
+// eslint-disable-next-line no-global-assign
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  for (var i = 0; i < entry.length; i++) {
+    newRequire(entry[i]);
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  return newRequire;
+})({"main.js":[function(require,module,exports) {
+var html = document.querySelector('#html');
+var style = document.querySelector('#style');
+var string = '\n/*\n *\u4F60\u597D,\u6211\u53EBCGC\n *\u63A5\u4E0B\u6765\u6211\u8981\u5C55\u793A\u6211\u7684\u524D\u7AEF\u529F\u5E95\n *\u9996\u5148\u6211\u8981\u51C6\u5907\u4E00\u4E2Adiv\n */\n#div1{\n    border:1px solid silver;\n    width: 200px;\n    height: 200px;\n}\n/* \u63A5\u4E0B\u6765,\u6211\u8981\u628A\u8FD9\u4E2Adiv\u53D8\u6210\u4E00\u4E2A\u592A\u6781\u56FE\n * \u6CE8\u610F\u770B\u597D\u4E86\n * \u9996\u5148,\u628Adiv\u53D8\u6210\u4E00\u4E2A\u5706\n */\n#div1 {\n    border-radius: 50%;\n    box-shadow: 1px 1px 5px 3px rgba(151, 145, 145, 0.4);\n    border: none\n}\n/* \u592A\u6781\u751F\u4E24\u4EEA\n * \u4E00\u9ED1\u4E00\u767D\n */\n\n#div1{\n    background: rgb(255,255,255);\n    background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 50%, rgba(0,0,0,1) 50%, rgba(0,0,0,1) 100%);\n}\n/*\n * \u589E\u52A0\u4E24\u4E2A\u9634\u9633\u9C7C\n*/\n\n#div1::before {\n    content: \' \';\n    display: block;\n    border-radius: 50%;\n    /* border:transparent 1px solid; */\n    height: 100px;\n    width: 100px;\n    position: relative;\n    top: 0px;\n    left: 50px;\n    background: rgb(255, 255, 255);\n    background: radial-gradient(circle, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 1) 15%, rgba(255, 255, 255, 1) 16%, rgba(255, 255, 255, 1) 100%);\n}\n\n#div1::after {\n    content: \' \';\n    display: block;\n    border-radius: 50%;\n    /* border: transparent 1px solid; */\n    height: 100px;\n    width: 100px;\n    position: relative;\n    top: 0px;\n    left: 50px;\n    background: rgb(0, 0, 0);\n    background: radial-gradient(circle, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 1) 15%, rgba(0, 0, 0, 1) 16%, rgba(0, 0, 0, 1) 100%);\n}\n\n/*   \u592A\u6781\u7ED8\u5236\u5B8C\u6210\n *   \u5C55\u793A\u7ED3\u675F\n */\n';
+
+var string2 = "";
+var n = 0;
+console.log(string.length);
+
+var step = function step() {
+
+    setTimeout(function () {
+
+        // 如果是回车,就把回车替换为<br>
+        //如果不是回车,就照搬string1
+        //另一种 解决输出undefined 的方法
+        // else if(string[n]===undefined){}  
+        string2 += string[n] === '\n' ? '<br>' : string[n] === ' ' ? '&ensp;' : string[n];
+        html.innerHTML = string2;
+        style.innerHTML = string.substring(0, n);
+        window.scrollTo(0, 99999);
+        html.scrollTo(0, 99999);
+        n += 1;
+        if (n < string.length) {
+            step();
+        }
+    }, 20);
+};
+
+step();
+},{}],"..\\..\\..\\..\\AppData\\Local\\Yarn\\Data\\global\\node_modules\\parcel\\src\\builtins\\hmr-runtime.js":[function(require,module,exports) {
+var global = arguments[3];
+var OVERLAY_ID = '__parcel__error__overlay__';
+
+var OldModule = module.bundle.Module;
+
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    data: module.bundle.hotData,
+    _acceptCallbacks: [],
+    _disposeCallbacks: [],
+    accept: function (fn) {
+      this._acceptCallbacks.push(fn || function () {});
+    },
+    dispose: function (fn) {
+      this._disposeCallbacks.push(fn);
+    }
+  };
+
+  module.bundle.hotData = null;
+}
+
+module.bundle.Module = Module;
+
+var parent = module.bundle.parent;
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = '' || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '54896' + '/');
+  ws.onmessage = function (event) {
+    var data = JSON.parse(event.data);
+
+    if (data.type === 'update') {
+      console.clear();
+
+      data.assets.forEach(function (asset) {
+        hmrApply(global.parcelRequire, asset);
+      });
+
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          hmrAccept(global.parcelRequire, asset.id);
+        }
+      });
+    }
+
+    if (data.type === 'reload') {
+      ws.close();
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+
+      removeErrorOverlay();
+    }
+
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
+
+      removeErrorOverlay();
+
+      var overlay = createErrorOverlay(data);
+      document.body.appendChild(overlay);
+    }
+  };
+}
+
+function removeErrorOverlay() {
+  var overlay = document.getElementById(OVERLAY_ID);
+  if (overlay) {
+    overlay.remove();
+  }
+}
+
+function createErrorOverlay(data) {
+  var overlay = document.createElement('div');
+  overlay.id = OVERLAY_ID;
+
+  // html encode message and stack trace
+  var message = document.createElement('div');
+  var stackTrace = document.createElement('pre');
+  message.innerText = data.error.message;
+  stackTrace.innerText = data.error.stack;
+
+  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
+
+  return overlay;
+}
+
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return [];
+  }
+
+  var parents = [];
+  var k, d, dep;
+
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(k);
+      }
+    }
+  }
+
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+
+  return parents;
+}
+
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+
+function hmrAccept(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+
+  if (!modules[id] && bundle.parent) {
+    return hmrAccept(bundle.parent, id);
+  }
+
+  var cached = bundle.cache[id];
+  bundle.hotData = {};
+  if (cached) {
+    cached.hot.data = bundle.hotData;
+  }
+
+  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+    cached.hot._disposeCallbacks.forEach(function (cb) {
+      cb(bundle.hotData);
+    });
+  }
+
+  delete bundle.cache[id];
+  bundle(id);
+
+  cached = bundle.cache[id];
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    cached.hot._acceptCallbacks.forEach(function (cb) {
+      cb();
+    });
+    return true;
+  }
+
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAccept(global.parcelRequire, id);
+  });
+}
+},{}]},{},["..\\..\\..\\..\\AppData\\Local\\Yarn\\Data\\global\\node_modules\\parcel\\src\\builtins\\hmr-runtime.js","main.js"], null)
+//# sourceMappingURL=/main.5f351abc.map
---
+++ src/main.js
@@ -0,0 +1,96 @@
+let html = document.querySelector('#html');
+let style = document.querySelector('#style');
+let string = `
+/*
+ *你好,我叫CGC
+ *接下来我要展示我的前端功底
+ *首先我要准备一个div
+ */
+#div1{
+    border:1px solid silver;
+    width: 200px;
+    height: 200px;
+}
+/* 接下来,我要把这个div变成一个太极图
+ * 注意看好了
+ * 首先,把div变成一个圆
+ */
+#div1 {
+    border-radius: 50%;
+    box-shadow: 1px 1px 5px 3px rgba(151, 145, 145, 0.4);
+    border: none
+}
+/* 太极生两仪
+ * 一黑一白
+ */
+
+#div1{
+    background: rgb(255,255,255);
+    background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 50%, rgba(0,0,0,1) 50%, rgba(0,0,0,1) 100%);
+}
+/*
+ * 增加两个阴阳鱼
+*/
+
+#div1::before {
+    content: ' ';
+    display: block;
+    border-radius: 50%;
+    /* border:transparent 1px solid; */
+    height: 100px;
+    width: 100px;
+    position: relative;
+    top: 0px;
+    left: 50px;
+    background: rgb(255, 255, 255);
+    background: radial-gradient(circle, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 1) 15%, rgba(255, 255, 255, 1) 16%, rgba(255, 255, 255, 1) 100%);
+}
+
+#div1::after {
+    content: ' ';
+    display: block;
+    border-radius: 50%;
+    /* border: transparent 1px solid; */
+    height: 100px;
+    width: 100px;
+    position: relative;
+    top: 0px;
+    left: 50px;
+    background: rgb(0, 0, 0);
+    background: radial-gradient(circle, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 1) 15%, rgba(0, 0, 0, 1) 16%, rgba(0, 0, 0, 1) 100%);
+}
+
+/*   太极绘制完成
+ *   展示结束
+ */
+`;
+
+let string2 = "";
+let n = 0
+console.log(string.length);
+
+
+let step = () => {
+
+    setTimeout(() => {
+
+        // 如果是回车,就把回车替换为<br>
+        //如果不是回车,就照搬string1
+        //另一种 解决输出undefined 的方法
+        // else if(string[n]===undefined){}  
+        string2 += (string[n] === '\n' ? '<br>' : (string[n] === ' ' ? '&ensp;' : string[n]))
+        html.innerHTML = string2;
+        style.innerHTML = string.substring(0, n);
+        window.scrollTo(0, 99999);
+        html.scrollTo(0, 99999);
+        n += 1;
+        if (n < string.length) {
+            step()
+        }
+
+    }, 20)
+
+}
+
+
+step()
